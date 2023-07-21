aa551ebe57801c69010815119fe21e19a858780c
melMass <mel@melmassadian.com>
2023-07-21 23:36:37 +0200

    feat: ✨ add FitNumber node
    quite useful in conjunction to AnimationBuilder

--- nodes/graph_utils.py
+++ nodes/graph_utils.py
@@ -94,4 +94,44 @@ class StringReplace:
         return (string,)
 
 
-__nodes__ = [SaveTensors, StringReplace]
+class FitNumber:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "value": ("FLOAT", {"default": 0, "forceInput": True}),
+                "clamp": ("BOOL", {"default": False}),
+                "source_min": ("FLOAT", {"default": 0.0}),
+                "source_max": ("FLOAT", {"default": 1.0}),
+                "target_min": ("FLOAT", {"default": 0.0}),
+                "target_max": ("FLOAT", {"default": 1.0}),
+            }
+        }
+
+    FUNCTION = "set_range"
+    RETURN_TYPES = ("FLOAT",)
+    CATEGORY = "mtb/math"
+
+    def set_range(
+        self,
+        value: float,
+        clamp: bool,
+        source_min: float,
+        source_max: float,
+        target_min: float,
+        target_max: float,
+    ):
+        res = target_min + (target_max - target_min) * (value - source_min) / (
+            source_max - source_min
+        )
+
+        if clamp:
+            if target_min > target_max:
+                res = max(min(res, target_min), target_max)
+            else:
+                res = max(min(res, target_max), target_min)
+
+        return (res,)
+
+
+__nodes__ = [SaveTensors, StringReplace, FitNumber]
