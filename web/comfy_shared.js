11862397514cfa076bac1d657c11903b9128dc22
Mel Massadian <mel@melmassadian.com>
2024-05-14 15:36:29 +0200

    chore 🧹: use sections properly

--- web/comfy_shared.js
+++ web/comfy_shared.js
@@ -12,11 +12,13 @@
 
 import { app } from '../../scripts/app.js'
 
+// #region base utils
+
 // - crude uuid
 export function makeUUID() {
   let dt = new Date().getTime()
   const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = ((dt + Math.random() * 16) % 16) | 0
+    const r = (dt + Math.random() * 16) % 16 | 0
     dt = Math.floor(dt / 16)
     return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
   })
@@ -70,8 +72,8 @@ function createLogger(emoji, color, consoleMethod = 'log') {
   }
 }
 
-export const infoLogger = createLogger('i', 'yellow')
-export const warnLogger = createLogger('!', 'orange', 'warn')
+export const infoLogger = createLogger('ℹ️', 'yellow')
+export const warnLogger = createLogger('⚠️', 'orange', 'warn')
 export const errorLogger = createLogger('🔥', 'red', 'error')
 export const successLogger = createLogger('✅', 'green')
 
@@ -103,8 +105,161 @@ export function deepMerge(target, ...sources) {
   return deepMerge(target, ...sources)
 }
 
-//- WIDGET UTILS
+// #endregion
+
+// #region widget utils
 export const CONVERTED_TYPE = 'converted-widget'
+
+export function hideWidget(node, widget, suffix = '') {
+  widget.origType = widget.type
+  widget.hidden = true
+  widget.origComputeSize = widget.computeSize
+  widget.origSerializeValue = widget.serializeValue
+  widget.computeSize = () => [0, -4] // -4 is due to the gap litegraph adds between widgets automatically
+  widget.type = CONVERTED_TYPE + suffix
+  widget.serializeValue = () => {
+    // Prevent serializing the widget if we have no input linked
+    const { link } = node.inputs.find((i) => i.widget?.name === widget.name)
+    if (link == null) {
+      return undefined
+    }
+    return widget.origSerializeValue
+      ? widget.origSerializeValue()
+      : widget.value
+  }
+
+  // Hide any linked widgets, e.g. seed+seedControl
+  if (widget.linkedWidgets) {
+    for (const w of widget.linkedWidgets) {
+      hideWidget(node, w, `:${widget.name}`)
+    }
+  }
+}
+
+/**
+ * Show widget
+ *
+ * @param {import("../../../web/types/litegraph.d.ts").IWidget} widget - target widget
+ */
+export function showWidget(widget) {
+  widget.type = widget.origType
+  widget.computeSize = widget.origComputeSize
+  widget.serializeValue = widget.origSerializeValue
+
+  delete widget.origType
+  delete widget.origComputeSize
+  delete widget.origSerializeValue
+
+  // Hide any linked widgets, e.g. seed+seedControl
+  if (widget.linkedWidgets) {
+    for (const w of widget.linkedWidgets) {
+      showWidget(w)
+    }
+  }
+}
+
+export function convertToWidget(node, widget) {
+  showWidget(widget)
+  const sz = node.size
+  node.removeInput(node.inputs.findIndex((i) => i.widget?.name === widget.name))
+
+  for (const widget of node.widgets) {
+    widget.last_y -= LiteGraph.NODE_SLOT_HEIGHT
+  }
+
+  // Restore original size but grow if needed
+  node.setSize([Math.max(sz[0], node.size[0]), Math.max(sz[1], node.size[1])])
+}
+
+export function convertToInput(node, widget, config) {
+  hideWidget(node, widget)
+
+  const { linkType } = getWidgetType(config)
+
+  // Add input and store widget config for creating on primitive node
+  const sz = node.size
+  node.addInput(widget.name, linkType, {
+    widget: { name: widget.name, config },
+  })
+
+  for (const widget of node.widgets) {
+    widget.last_y += LiteGraph.NODE_SLOT_HEIGHT
+  }
+
+  // Restore original size but grow if needed
+  node.setSize([Math.max(sz[0], node.size[0]), Math.max(sz[1], node.size[1])])
+}
+
+export function hideWidgetForGood(node, widget, suffix = '') {
+  widget.origType = widget.type
+  widget.origComputeSize = widget.computeSize
+  widget.origSerializeValue = widget.serializeValue
+  widget.computeSize = () => [0, -4] // -4 is due to the gap litegraph adds between widgets automatically
+  widget.type = CONVERTED_TYPE + suffix
+  // widget.serializeValue = () => {
+  //     // Prevent serializing the widget if we have no input linked
+  //     const w = node.inputs?.find((i) => i.widget?.name === widget.name);
+  //     if (w?.link == null) {
+  //         return undefined;
+  //     }
+  //     return widget.origSerializeValue ? widget.origSerializeValue() : widget.value;
+  // };
+
+  // Hide any linked widgets, e.g. seed+seedControl
+  if (widget.linkedWidgets) {
+    for (const w of widget.linkedWidgets) {
+      hideWidgetForGood(node, w, `:${widget.name}`)
+    }
+  }
+}
+
+export function fixWidgets(node) {
+  if (node.inputs) {
+    for (const input of node.inputs) {
+      log(input)
+      if (input.widget || node.widgets) {
+        // if (newTypes.includes(input.type)) {
+        const matching_widget = node.widgets.find((w) => w.name === input.name)
+        if (matching_widget) {
+          // if (matching_widget.hidden) {
+          //     log(`Already hidden skipping ${matching_widget.name}`)
+          //     continue
+          // }
+          const w = node.widgets.find((w) => w.name === matching_widget.name)
+          if (w && w.type !== CONVERTED_TYPE) {
+            log(w)
+            log(`hidding ${w.name}(${w.type}) from ${node.type}`)
+            log(node)
+            hideWidget(node, w)
+          } else {
+            log(`converting to widget ${w}`)
+
+            convertToWidget(node, input)
+          }
+        }
+      }
+    }
+  }
+}
+export function inner_value_change(widget, val, event = undefined) {
+  let value = val
+  if (widget.type === 'number' || widget.type === 'BBOX') {
+    value = Number(value)
+  } else if (widget.type === 'BOOL') {
+    value = Boolean(value)
+  }
+  widget.value = corrected_value
+  if (
+    widget.options?.property &&
+    node.properties[widget.options.property] !== undefined
+  ) {
+    node.setProperty(widget.options.property, value)
+  }
+  if (widget.callback) {
+    widget.callback(widget.value, app.canvas, node, pos, event)
+  }
+}
+
 /**
  * @param {LGraphNode} node
  * @param {LLink} link
@@ -136,7 +291,6 @@ export const cleanupNode = (node) => {
   if (!hasWidgets(node)) {
     return
   }
-
   for (const w of node.widgets) {
     if (w.canvas) {
       w.canvas.remove()
@@ -200,6 +354,9 @@ export function getWidgetType(config) {
   return { type, linkType }
 }
 
+// #endregion
+
+// #region dynamic connections
 /**
  * @param {NodeType} nodeType
  * @param {str} prefix
@@ -388,6 +545,25 @@ export const dynamic_connection = (
     }
   }
 }
+// #endregion
+
+// #region color utils
+export function isColorBright(rgb, threshold = 240) {
+  const brightess = getBrightness(rgb)
+  return brightess > threshold
+}
+
+function getBrightness(rgbObj) {
+  return Math.round(
+    (Number.parseInt(rgbObj[0]) * 299 +
+      Number.parseInt(rgbObj[1]) * 587 +
+      Number.parseInt(rgbObj[2]) * 114) /
+      1000,
+  )
+}
+// #endregion
+
+// #region html/css utils
 
 /**
  * Calculate total height of DOM element child
@@ -414,188 +590,7 @@ export function calculateTotalChildrenHeight(parentElement) {
 
   return totalHeight
 }
-/**
- * Appends a callback to the extra menu options of a given node type.
- * @param {NodeType} nodeType
- * @param {(app,options) => ContextMenuItem[]} cb
- */
-export function addMenuHandler(nodeType, cb) {
-  const getOpts = nodeType.prototype.getExtraMenuOptions
-  /**
-   * @returns {ContextMenuItem[]} items
-   */
-  nodeType.prototype.getExtraMenuOptions = function (app, options) {
-    const r = getOpts.apply(this, [app, options]) || []
-    const newItems = cb.apply(this, [app, options])
-    return r + newItems
-  }
-}
 
-export function hideWidget(node, widget, suffix = '') {
-  widget.origType = widget.type
-  widget.hidden = true
-  widget.origComputeSize = widget.computeSize
-  widget.origSerializeValue = widget.serializeValue
-  widget.computeSize = () => [0, -4] // -4 is due to the gap litegraph adds between widgets automatically
-  widget.type = CONVERTED_TYPE + suffix
-  widget.serializeValue = () => {
-    // Prevent serializing the widget if we have no input linked
-    const { link } = node.inputs.find((i) => i.widget?.name === widget.name)
-    if (link == null) {
-      return undefined
-    }
-    return widget.origSerializeValue
-      ? widget.origSerializeValue()
-      : widget.value
-  }
-
-  // Hide any linked widgets, e.g. seed+seedControl
-  if (widget.linkedWidgets) {
-    for (const w of widget.linkedWidgets) {
-      hideWidget(node, w, `:${widget.name}`)
-    }
-  }
-}
-
-/**
- * Show widget
- *
- * @param {import("../../../web/types/litegraph.d.ts").IWidget} widget - target widget
- */
-export function showWidget(widget) {
-  widget.type = widget.origType
-  widget.computeSize = widget.origComputeSize
-  widget.serializeValue = widget.origSerializeValue
-
-  delete widget.origType
-  delete widget.origComputeSize
-  delete widget.origSerializeValue
-
-  // Hide any linked widgets, e.g. seed+seedControl
-  if (widget.linkedWidgets) {
-    for (const w of widget.linkedWidgets) {
-      showWidget(w)
-    }
-  }
-}
-
-export function convertToWidget(node, widget) {
-  showWidget(widget)
-  const sz = node.size
-  node.removeInput(node.inputs.findIndex((i) => i.widget?.name === widget.name))
-
-  for (const widget of node.widgets) {
-    widget.last_y -= LiteGraph.NODE_SLOT_HEIGHT
-  }
-
-  // Restore original size but grow if needed
-  node.setSize([Math.max(sz[0], node.size[0]), Math.max(sz[1], node.size[1])])
-}
-
-export function convertToInput(node, widget, config) {
-  hideWidget(node, widget)
-
-  const { linkType } = getWidgetType(config)
-
-  // Add input and store widget config for creating on primitive node
-  const sz = node.size
-  node.addInput(widget.name, linkType, {
-    widget: { name: widget.name, config },
-  })
-
-  for (const widget of node.widgets) {
-    widget.last_y += LiteGraph.NODE_SLOT_HEIGHT
-  }
-
-  // Restore original size but grow if needed
-  node.setSize([Math.max(sz[0], node.size[0]), Math.max(sz[1], node.size[1])])
-}
-
-export function hideWidgetForGood(node, widget, suffix = '') {
-  widget.origType = widget.type
-  widget.origComputeSize = widget.computeSize
-  widget.origSerializeValue = widget.serializeValue
-  widget.computeSize = () => [0, -4] // -4 is due to the gap litegraph adds between widgets automatically
-  widget.type = CONVERTED_TYPE + suffix
-  // widget.serializeValue = () => {
-  //     // Prevent serializing the widget if we have no input linked
-  //     const w = node.inputs?.find((i) => i.widget?.name === widget.name);
-  //     if (w?.link == null) {
-  //         return undefined;
-  //     }
-  //     return widget.origSerializeValue ? widget.origSerializeValue() : widget.value;
-  // };
-
-  // Hide any linked widgets, e.g. seed+seedControl
-  if (widget.linkedWidgets) {
-    for (const w of widget.linkedWidgets) {
-      hideWidgetForGood(node, w, `:${widget.name}`)
-    }
-  }
-}
-
-export function fixWidgets(node) {
-  if (node.inputs) {
-    for (const input of node.inputs) {
-      log(input)
-      if (input.widget || node.widgets) {
-        // if (newTypes.includes(input.type)) {
-        const matching_widget = node.widgets.find((w) => w.name === input.name)
-        if (matching_widget) {
-          // if (matching_widget.hidden) {
-          //     log(`Already hidden skipping ${matching_widget.name}`)
-          //     continue
-          // }
-          const w = node.widgets.find((w) => w.name === matching_widget.name)
-          if (w && w.type !== CONVERTED_TYPE) {
-            log(w)
-            log(`hidding ${w.name}(${w.type}) from ${node.type}`)
-            log(node)
-            hideWidget(node, w)
-          } else {
-            log(`converting to widget ${w}`)
-
-            convertToWidget(node, input)
-          }
-        }
-      }
-    }
-  }
-}
-export function inner_value_change(widget, val, event = undefined) {
-  let value = val
-  if (widget.type === 'number' || widget.type === 'BBOX') {
-    value = Number(value)
-  } else if (widget.type === 'BOOL') {
-    value = Boolean(value)
-  }
-  widget.value = corrected_value
-  if (
-    widget.options?.property &&
-    node.properties[widget.options.property] !== undefined
-  ) {
-    node.setProperty(widget.options.property, value)
-  }
-  if (widget.callback) {
-    widget.callback(widget.value, app.canvas, node, pos, event)
-  }
-}
-
-//- COLOR UTILS
-export function isColorBright(rgb, threshold = 240) {
-  const brightess = getBrightness(rgb)
-  return brightess > threshold
-}
-
-function getBrightness(rgbObj) {
-  return Math.round(
-    (Number.parseInt(rgbObj[0]) * 299 +
-      Number.parseInt(rgbObj[1]) * 587 +
-      Number.parseInt(rgbObj[2]) * 114) /
-      1000,
-  )
-}
-//- HTML / CSS UTILS
 export const loadScript = (
   FILE_URL,
   async = true,
@@ -666,22 +661,9 @@ export function defineClass(className, classStyles) {
   }
 }
 
-/** Prefixes the node title with '[DEPRECATED]' and log the deprecation reason to the console.*/
-export const addDeprecation = (nodeType, reason) => {
-  const title = nodeType.title
-  nodeType.title = `[DEPRECATED] ${title}`
-  // console.log(nodeType)
+// #endregion
 
-  const styles = {
-    title: 'font-size:1.3em;font-weight:900;color:yellow; background: black',
-    reason: 'font-size:1.2em',
-  }
-  console.log(
-    `%c!  ${title} is deprecated:%c ${reason}`,
-    styles.title,
-    styles.reason,
-  )
-}
+// #region documentation widget
 
 const create_documentation_stylesheet = () => {
   const tag = 'mtb-documentation-stylesheet'
@@ -1002,4 +984,87 @@ export const addDocumentation = (
 
     // return r;
   }
+}
+
+// #endregion
+
+// #region node extensions
+
+/**
+ * Extend an object, either replacing the original property or extending it.
+ * @param {Object} object - The object to which the property belongs.
+ * @param {string} property - The name of the property to chain the callback to.
+ * @param {Function} callback - The callback function to be chained.
+ */
+export function extendPrototype(object, property, callback) {
+  if (object === undefined) {
+    console.error('Could not extend undefined object', { object, property })
+    return
+  }
+  if (property in object) {
+    const callback_orig = object[property]
+    object[property] = function (...args) {
+      const r = callback_orig.apply(this, args)
+      callback.apply(this, args)
+      return r
+    }
+  } else {
+    object[property] = callback
+  }
+}
+
+/**
+ * Appends a callback to the extra menu options of a given node type.
+ * @param {NodeType} nodeType
+ * @param {(app,options) => ContextMenuItem[]} cb
+ */
+export function addMenuHandler(nodeType, cb) {
+  const getOpts = nodeType.prototype.getExtraMenuOptions
+  /**
+   * @returns {ContextMenuItem[]} items
+   */
+  nodeType.prototype.getExtraMenuOptions = function (app, options) {
+    const r = getOpts.apply(this, [app, options]) || []
+    const newItems = cb.apply(this, [app, options])
+    return r + newItems
+  }
+}
+
+/** Prefixes the node title with '[DEPRECATED]' and log the deprecation reason to the console.*/
+export const addDeprecation = (nodeType, reason) => {
+  const title = nodeType.title
+  nodeType.title = `[DEPRECATED] ${title}`
+  // console.log(nodeType)
+
+  const styles = {
+    title: 'font-size:1.3em;font-weight:900;color:yellow; background: black',
+    reason: 'font-size:1.2em',
+  }
+  console.log(
+    `%c!  ${title} is deprecated:%c ${reason}`,
+    styles.title,
+    styles.reason,
+  )
+}
+
+// #endregion
+
+// #region graph utilities
+export const getNodes = (skip_unused) => {
+  const nodes = []
+  for (const outerNode of app.graph.computeExecutionOrder(false)) {
+    const skipNode =
+      (outerNode.mode === 2 || outerNode.mode === 4) && skip_unused
+    const innerNodes =
+      !skipNode && outerNode.getInnerNodes
+        ? outerNode.getInnerNodes()
+        : [outerNode]
+    for (const node of innerNodes) {
+      if ((node.mode === 2 || node.mode === 4) && skip_unused) {
+        continue
+      }
+      nodes.push(node)
+    }
+  }
+  return nodes
 }
