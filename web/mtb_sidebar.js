b0e234b7eef438f1e020bc6843af9a6f5466c421
Mel Massadian <mel@melmassadian.com>
2024-08-18 14:11:34 +0200

    feat: ✨ dump of wip things...

---
+++ web/mtb_sidebar.js
@@ -0,0 +1,25 @@
+import { app } from '../../scripts/app.js'
+import { api } from '../../scripts/api.js'
+
+import * as shared from './comfy_shared.js'
+import { createOutliner } from './dist/mtb_inspector.js'
+
+if (window?.__COMFYUI_FRONTEND_VERSION__) {
+  const version = window?.__COMFYUI_FRONTEND_VERSION__
+  console.log(`%c ${version}`, 'background: orange; color: white;')
+
+  const panel = app.extensionManager.registerSidebarTab({
+    id: 'mtb-nodes',
+    icon: 'pi pi-bolt',
+    title: 'MTB',
+    tooltip: 'sidebar for mtb nodes',
+    type: 'custom',
+    // this is run everytime the tab's diplay is toggled on.
+    render: (el) => {
+      const outliner = createOutliner(el)
+      const inputs = shared.getAPIInputs()
+      console.log('INPUTS', inputs)
+      outliner.$$set({ inputs })
+    },
+  })
+}
