3d12bd29a86bf650e51135fb375a28f750fec21d
Mel Massadian <mel@melmassadian.com>
2025-08-01 17:26:58 +0200

    chore: 🧹 add back convex helper

--- web/comfy_shared.js
+++ web/comfy_shared.js
@@ -14,6 +14,51 @@ import { api } from '../../scripts/api.js'
 
 // #region base utils
 
+/**
+ * Computes the convex hull of a set of points using the Monotone Chain algorithm.
+ *
+ * @param {Array<Array<number>>} points An array of points, where each point is an array of two numbers [x, y].
+ * @returns {Array<Array<number>>} The points forming the convex hull, in counter-clockwise order.
+ */
+export const getConvexHull = (points) => {
+  if (points.length <= 3) {
+    return points
+  }
+
+  points.sort((a, b) => a[0] - b[0] || a[1] - b[1])
+
+  const lower = []
+  for (const p of points) {
+    while (
+      lower.length >= 2 &&
+      cross_product(lower[lower.length - 2], lower[lower.length - 1], p) <= 0
+    ) {
+      lower.pop()
+    }
+    lower.push(p)
+  }
+
+  const upper = []
+  for (let i = points.length - 1; i >= 0; i--) {
+    const p = points[i]
+    while (
+      upper.length >= 2 &&
+      cross_product(upper[upper.length - 2], upper[upper.length - 1], p) <= 0
+    ) {
+      upper.pop()
+    }
+    upper.push(p)
+  }
+
+  function cross_product(o, a, b) {
+    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+  }
+
+  return lower
+    .slice(0, lower.length - 1)
+    .concat(upper.slice(0, upper.length - 1))
+}
+
 // - crude uuid
 export function makeUUID() {
   let dt = new Date().getTime()
